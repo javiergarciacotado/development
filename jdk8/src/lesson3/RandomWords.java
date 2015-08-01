c6eeaf956577195a0eaac14d2fa6afa7d6677fed
jgarciacotado <jgarciacotado@gmail.com>
2015-08-01 20:50:52 +0100

    Added third JDK8 MOOC course and refactor

---
+++ jdk8/src/lesson3/RandomWords.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright © 2014, Oracle and/or its affiliates. All rights reserved.
+ * 
+ * JDK 8 MOOC Lesson 3 homework
+ */
+package lesson3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * Class to generate a list of random words
+ *
+ * @author Simon Ritter (@speakjava)
+ */
+public class RandomWords {
+  private final List<String> sourceWords;
+
+  /**
+   * Constructor
+   * 
+   * @throws IOException If the source words file cannot be read
+   */
+  public RandomWords() throws IOException {   
+    try (BufferedReader reader = Files.newBufferedReader(Paths.get("./jdk8/src/lesson3/words.txt"))) {
+
+      // YOUR CODE HERE
+      sourceWords = reader.lines().collect(toList());
+      System.out.println("Loaded " + sourceWords.size() + " words");
+    }
+  }
+
+  /**
+   * Create a list of a given size containing random words
+   *
+   * @param listSize The size of the list to create
+   * @return The created list
+   */
+  public List<String> createList(int listSize) {
+    Random rand = new Random();
+    // YOUR CODE HERE
+    return rand.ints(listSize,0,sourceWords.size())
+              .parallel()
+              .mapToObj(s -> sourceWords.get(s))
+              .collect(Collectors.toList());
+  }
+
+  /**
+   * Return the list of all source words, which cannot be modified
+   *
+   * @return The unmodifiable list of all source words
+   */
+  public List<String> allWords() {
+    return Collections.unmodifiableList(sourceWords);
+  }
+}
